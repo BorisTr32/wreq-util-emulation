9e2a6218d540cfdd15e00f817a99b625553dbabd
BorisTr32 <troshinboris1977@gmail.com>
2025-09-13 06:18:23 +0400

    Update mod.rs v142

--- src/emulation/mod.rs
+++ src/emulation/mod.rs
@@ -351,6 +351,7 @@ impl wreq::EmulationFactory for EmulationOption {
             Emulation::Firefox136 => ff136::emulation,
             Emulation::FirefoxPrivate136 => ff_private_136::emulation,
             Emulation::Firefox139 => ff139::emulation,
+            Emulation::Firefox142 => ff142::emulation,
 
             Emulation::Opera116 => opera116::emulation,
             Emulation::Opera117 => opera117::emulation,
