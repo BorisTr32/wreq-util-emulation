f6911442db367603b0f7f166016bfceced9e9c79
BorisTr32 <troshinboris1977@gmail.com>
2025-11-01 00:15:11 +0400

    Add support for Chrome version 142

--- src/emulation/mod.rs
+++ src/emulation/mod.rs
@@ -127,6 +127,7 @@ define_enum!(
     Chrome139 => ("chrome_139", v139::emulation),
     Chrome140 => ("chrome_140", v140::emulation),
     Chrome141 => ("chrome_141", v141::emulation),
+    Chrome142 => ("chrome_142", v142::emulation),
 
     // Edge versions
     Edge101 => ("edge_101", edge101::emulation),
